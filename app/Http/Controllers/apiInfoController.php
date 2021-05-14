ed7ab30b9869f2487a557ffd7ce6beb81548a1f4
Thuan <thuansilva14@gmail.com>
2021-05-13 20:51:32 -0400

    Configuration

--- app/Http/Controllers/apiInfoController.php
+++ app/Http/Controllers/apiInfoController.php
@@ -1,14 +1,11 @@
 <?php
-
-// $dotenv = Dotenv\Dotenv :: createImmutable (__DIR__);
-// $dotenv -> load ();
-
 namespace App\Http\Controllers;
-
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\DB;
+
 class apiInfoController extends Controller
 {
+
     public  function index(){
         $response = Http::get("https://g1.globo.com/rss/g1/");
         sleep(1);
@@ -18,15 +15,12 @@ class apiInfoController extends Controller
     }
 
     public function api (){
-        $token ='25MjHjLY7fSu54HrILcExdrUba5h2UZZKwrQT6YSBRJjLtoyg7pg4iwL2tKY';
+        $token = $_ENV['TOKEN_ZAPITO'];
         $data = [
                'test_mode'=> true,
                'data'=>
                     $this->getDadosBd(),
-
-
         ];
-
         $response= Http::withHeaders([
             'Authorization' => 'Bearer '.$token,
             'accept' => 'application/json'])->post('https://zapito.com.br/api/messages',$data);
@@ -34,28 +28,21 @@ class apiInfoController extends Controller
             echo  $response->getStatusCode();
     }
 
-
-
     public function getDadosBd (){
         $users =(Array) DB::select('SELECT *
         FROM `tags`
         where `status` = "Ativo"');
 
         $count = DB::table('tags')->select('*')->where('status', "Ativo")->count('*');
-
-        // print_r($count);
-
         for ($i=0; $i< $count;$i++){
            $item[$i]=[
                 'phone' => $users[$i]->telefone,
-                 'message'=> '*'.$users[$i]->name.'*'.', olha esssa isso: '.$this->index(),
-                "test_mode"=> true
+                'message'=> '*'.$users[$i]->name.'*'.', olha esssa isso: '.$this->index(),
+                'test_mode'=> true
             ];
 
         }
         return $item;
-        print_r($item);
-
     }
 
 }
